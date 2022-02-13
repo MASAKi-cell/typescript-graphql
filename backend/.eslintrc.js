a6c80e01b611307dffc44555110be84d7feeeb70
MASAKi-cell <bje35149@gmail.com>
2022-02-13 15:45:34 +0900

    eslintrcの設定

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -1,20 +1,54 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "extends": [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended"
+    extends: [
+      'eslint:recommended',
+      'plugin:@typescript-eslint/eslint-recommended',
+      'plugin:@typescript-eslint/recommended',
+      'prettier',
+      'prettier/@typescript-eslint',
     ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
+    plugins: ['@typescript-eslint'],
+    parser: '@typescript-eslint/parser',
+    parserOptions: { sourceType: 'module', ecmaVersion: 2020 },
+    env: { es6: true, node: true },
+    globals: {},
+    settings: {
+      'import/resolver': {
+        node: {
+          extensions: ['.js', '.jsx', '.json', '.ts', '.tsx'],
+        },
+        typescript: {},
+      },
     },
-    "plugins": [
-        "@typescript-eslint"
-    ],
-    "rules": {
-    }
-}
+    ignorePatterns: ['tests/**/*.ts'],
+    rules: {
+      '@typescript-eslint/ban-types': [
+        'error',
+        {
+          types: {
+            String: false,
+            Boolean: false,
+            Number: false,
+            Symbol: false,
+            '{}': false,
+            Object: false,
+            object: false,
+            Function: false,
+          },
+          extendDefaults: true,
+        },
+      ],
+      '@typescript-eslint/member-delimiter-style': [
+        'error',
+        {
+          multiline: {
+            delimiter: 'none',
+            requireLast: false,
+          },
+          singleline: {
+            delimiter: 'comma',
+            requireLast: false,
+          },
+        },
+      ],
+    },
+  }
